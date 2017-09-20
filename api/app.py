e0d65aa703693ad2e0faa89944865782a24923a3
Simon Waloschek <simon.waloschek@posteo.de>
2017-09-20 09:06:53 +0200

    Temporarily added verovio

--- api/app.py
+++ api/app.py
@@ -240,4 +240,4 @@ def get_alignment_from_yt(body, response):
 
 
 if __name__ == '__main__':
-    hug.API(__name__).http.serve(port=8000)
+    hug.API(__name__).http.serve(host='0.0.0.0', port=8000)
